6a951706e593678624a77a98ec0dd01e15a640b7
palhamel <pal.hamel@gmail.com>
2020-03-04 19:22:29 +0100

    Trying even more..

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -4,5 +4,24 @@ const months = [
 ]
 
 export const monthName = (monthNumber) => {
+
+  if (monthNumber < 1 || monthNumber > 12) {
+    return null
+  }
   return months[monthNumber]
 }
+
+
+/*
+
+const months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+
+export const monthName = (monthNumber) => {
+  return months[monthNumber]
+}
+
+
+*/
